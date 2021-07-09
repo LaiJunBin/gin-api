54d5ecebd4c940ddc5cd9337809d0fa593308af7
laijunbin <xyz607xx@gmail.com>
2021-07-09 21:50:15 +0800

    Update password column hidden.

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,7 +6,7 @@ type User struct {
 	gorm.Model
 	Name string `gorm:"not null" json:"name,omitempty"`
 	Username string `gorm:"not null; unique" json:"username,omitempty"`
-	Password string `gorm:"not null" json:"password,omitempty"`
+	Password string `gorm:"not null" json:"-"`
 }
 
 func (u User) Create(db *gorm.DB) (User, error) {
